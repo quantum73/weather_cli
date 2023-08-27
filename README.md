24686943ff1b204540b383d1bf4abbed49091897
Nikita Fomin <fominquant@yandex.ru>
2023-08-27 20:59:25 +0300

    - Updated README - Minor fixes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather_cli
+
+Execute `python3 main.py` command in root dir. 
+
+Console output:
+
+![output example](./output_example.png)
--- services/printing.py
+++ services/printing.py
@@ -6,5 +6,5 @@ def pretty_print_weather(*, weather: Weather) -> None:
         f"Дата и время: {weather.now_datetime}\n"
         f"Место: {weather.geo_name}\n"
         f"Температура: {weather.temperature}°C\n"
-        f"На улице: {weather.weather_title}"
+        f"На улице: {weather.weather_title.value}"
     )
